cd791ad61e3c1510973812b47646f6238cce2f20
JianmingTONG <573925529@qq.com>
2021-04-21 10:40:05 +0800

    update 16:8 NoC

--- topology/crossbar_one_hot_seq.v
+++ topology/crossbar_one_hot_seq.v
@@ -69,13 +69,13 @@
 // `define In16_Out8 
 // `define In8_Out8 
 // `define In8_Out8_input_tree_wire_pipeline_hierarchy_RTL 
-`define In8_Out8_input_tree_wire_pipeline_hierarchy_RTL_ease_pnr // -> lastest 8:8 version
+// `define In8_Out8_input_tree_wire_pipeline_hierarchy_RTL_ease_pnr // -> lastest 8:8 version
 // `define In8_Out8_input_tree_wire_pipeline_hierarchy_RTL_output_pipeline
 // `define In8_Out1
 // `define In8_Out8_input_pipeline_output_pipeline
 // `define In16_Out8_separate_8_8
 // `define In16_Out8_separate_8_8_wire_pipeline
-// `define In16_Out8_8_8_wire_tree_pipeline   // -> lastest 16:8 version 
+`define In16_Out8_8_8_wire_tree_pipeline   // -> lastest 16:8 version 
 // `define In16_Out8_pipeline_wire
 // `define In16_Out8_pipeline_wire_pipeline_i_en_pipeline_mux
 // `define In16_Out8_pipeline_wire_pipeline_i_en_pipeline_mux_output_pipeline
@@ -2653,7 +2653,7 @@ endmodule
 
 `ifdef In16_Out8_8_8_wire_tree_pipeline
 module crossbar_one_hot_seq#(
-	parameter DATA_WIDTH = 32,      // could be arbitrary number
+	parameter DATA_WIDTH = 4,      // could be arbitrary number
 	parameter NUM_OUTPUT_DATA  = 8, // must be power of 2.
 	parameter NUM_INPUT_DATA = 16   
 )(
@@ -2678,7 +2678,6 @@ module crossbar_one_hot_seq#(
 	localparam WIDTH_INPUT_DATA = NUM_INPUT_DATA*DATA_WIDTH;
 	localparam WIDTH_OUTPUT_DATA = NUM_OUTPUT_DATA*DATA_WIDTH;
 	
-    
     localparam NUM_SEPARATE_FIRST_STAGE = 2;
 	
     // interface
@@ -2702,9 +2701,7 @@ module crossbar_one_hot_seq#(
         // define output reg of the two 8:8 mux.
         for(k=0; k<NUM_SEPARATE_FIRST_STAGE; k=k+1)
         begin:first_stage_output_def
-            // reg    [NUM_OUTPUT_DATA-1:0]                 o_valid_reg;
             wire   [NUM_OUTPUT_DATA-1:0]                 o_valid_wire;
-            // reg    [WIDTH_OUTPUT_DATA-1:0]               o_data_bus_reg;
             wire   [WIDTH_OUTPUT_DATA-1:0]               o_data_bus_wire;
         end
 
@@ -2717,7 +2714,7 @@ module crossbar_one_hot_seq#(
             crossbar_8_8_seq#(
                 .DATA_WIDTH(DATA_WIDTH),      // could be arbitrary number
                 .NUM_OUTPUT_DATA(NUM_OUTPUT_DATA), // must be power of 2.
-                .NUM_INPUT_DATA(NUM_INPUT_DATA)   
+                .NUM_INPUT_DATA(8)   
             ) xba8_8(
                 .clk(clk),
                 .rst(rst),
@@ -2820,7 +2817,7 @@ module crossbar_8_8_seq#(
 	
 	localparam WIDTH_INPUT_DATA = NUM_INPUT_DATA*DATA_WIDTH;
 	localparam WIDTH_OUTPUT_DATA = NUM_OUTPUT_DATA*DATA_WIDTH;
-	
+    
     // interface
 	input                                        clk;
 	input                                        rst;
@@ -2834,19 +2831,14 @@ module crossbar_8_8_seq#(
 	input                                        i_en;
 	input  [TOTAL_COMMMAND-1:0]                  i_cmd;
 
-	reg    [NUM_OUTPUT_DATA-1:0]                 o_valid_reg;             
-	reg    [WIDTH_OUTPUT_DATA-1:0]               o_data_bus_reg; // {o_data_a, o_data_b}
-
-    wire   [NUM_OUTPUT_DATA-1:0]                 o_valid_wire;             
-	wire   [WIDTH_OUTPUT_DATA-1:0]               o_data_bus_wire; // {o_data_a, o_data_b}
-
 
     // inner logic
     genvar i,j,k;
     generate
         // wire definition -- for input data & valid
-        wire [NUM_OUTPUT_DATA*NUM_INPUT_DATA*DATA_WIDTH-1:0]  inner_data_wire;
-        wire [NUM_OUTPUT_DATA*NUM_INPUT_DATA-1:0]             inner_valid_wire;
+        wire [NUM_OUTPUT_DATA*NUM_INPUT_DATA*DATA_WIDTH-1:0]  o_inner_data_wire;
+        wire [NUM_OUTPUT_DATA*NUM_INPUT_DATA-1:0]             o_inner_valid_wire;
+        wire [NUM_OUTPUT_DATA*NUM_INPUT_DATA-1:0]             o_inner_cmd_wire;
 
         // wire_tree_pipeline -- for input data & valid
         for(i=0; i<NUM_INPUT_DATA; i=i+1)
@@ -2860,8 +2852,8 @@ module crossbar_8_8_seq#(
                 .rst(rst),
                 .i_valid(i_valid[i]),
                 .i_data_bus(i_data_bus[i*DATA_WIDTH+:DATA_WIDTH]),
-                .o_valid(inner_valid_wire[i*NUM_OUTPUT_DATA+:NUM_OUTPUT_DATA]),
-                .o_data_bus(inner_data_wire[i*NUM_OUTPUT_DATA*DATA_WIDTH+:NUM_OUTPUT_DATA*DATA_WIDTH]),
+                .o_valid({o_inner_valid_wire[7*NUM_OUTPUT_DATA+i], o_inner_valid_wire[6*NUM_OUTPUT_DATA+i], o_inner_valid_wire[5*NUM_OUTPUT_DATA+i], o_inner_valid_wire[4*NUM_OUTPUT_DATA+i], o_inner_valid_wire[3*NUM_OUTPUT_DATA+i], o_inner_valid_wire[2*NUM_OUTPUT_DATA+i], o_inner_valid_wire[1*NUM_OUTPUT_DATA+i], o_inner_valid_wire[0*NUM_OUTPUT_DATA+i]}),
+                .o_data_bus({o_inner_data_wire[(7*NUM_OUTPUT_DATA+i)*DATA_WIDTH+:DATA_WIDTH], o_inner_data_wire[(6*NUM_OUTPUT_DATA+i)*DATA_WIDTH+:DATA_WIDTH], o_inner_data_wire[(5*NUM_OUTPUT_DATA+i)*DATA_WIDTH+:DATA_WIDTH], o_inner_data_wire[(4*NUM_OUTPUT_DATA+i)*DATA_WIDTH+:DATA_WIDTH], o_inner_data_wire[(3*NUM_OUTPUT_DATA+i)*DATA_WIDTH+:DATA_WIDTH], o_inner_data_wire[(2*NUM_OUTPUT_DATA+i)*DATA_WIDTH+:DATA_WIDTH], o_inner_data_wire[(1*NUM_OUTPUT_DATA+i)*DATA_WIDTH+:DATA_WIDTH], o_inner_data_wire[(0*NUM_OUTPUT_DATA+i)*DATA_WIDTH+:DATA_WIDTH]}),
                 .i_en(i_en)
             );
         end
@@ -2869,7 +2861,7 @@ module crossbar_8_8_seq#(
         // input command
         for(i=0; i<NUM_INPUT_DATA; i=i+1)
         begin:i_cmd_id  
-            wire                                             inner_cmd_wire[0:NUM_INPUT_DATA-1];
+            // wire                                             o_inner_cmd_wire[0:NUM_INPUT_DATA-1];
             
             cmd_wire_binary_tree_1_8_seq #(
                 .DATA_WIDTH(DATA_WIDTH), 
@@ -2879,14 +2871,14 @@ module crossbar_8_8_seq#(
                 .clk(clk),
                 .rst(rst),
                 .i_cmd(i_cmd[i*NUM_OUTPUT_DATA+:NUM_OUTPUT_DATA]),
-                .o_cmd_0(inner_cmd_wire[0]),
-                .o_cmd_1(inner_cmd_wire[1]),
-                .o_cmd_2(inner_cmd_wire[2]),
-                .o_cmd_3(inner_cmd_wire[3]),
-                .o_cmd_4(inner_cmd_wire[4]),
-                .o_cmd_5(inner_cmd_wire[5]),
-                .o_cmd_6(inner_cmd_wire[6]),
-                .o_cmd_7(inner_cmd_wire[7]),
+                .o_cmd_0(o_inner_cmd_wire[0*NUM_OUTPUT_DATA+i]),
+                .o_cmd_1(o_inner_cmd_wire[1*NUM_OUTPUT_DATA+i]),
+                .o_cmd_2(o_inner_cmd_wire[2*NUM_OUTPUT_DATA+i]),
+                .o_cmd_3(o_inner_cmd_wire[3*NUM_OUTPUT_DATA+i]),
+                .o_cmd_4(o_inner_cmd_wire[4*NUM_OUTPUT_DATA+i]),
+                .o_cmd_5(o_inner_cmd_wire[5*NUM_OUTPUT_DATA+i]),
+                .o_cmd_6(o_inner_cmd_wire[6*NUM_OUTPUT_DATA+i]),
+                .o_cmd_7(o_inner_cmd_wire[7*NUM_OUTPUT_DATA+i]),
                 .i_en(i_en)
             );
         end
@@ -2898,9 +2890,9 @@ module crossbar_8_8_seq#(
             wire [NUM_INPUT_DATA-1:0]             inner_valid_i_mux_tree_wire;
             wire [NUM_OUTPUT_DATA-1:0]            inner_cmd_i_mux_tree_wire;
 
-            assign inner_data_i_mux_tree_wire = {inner_data_wire[(7*NUM_OUTPUT_DATA+i)*DATA_WIDTH+:DATA_WIDTH], inner_data_wire[(6*NUM_OUTPUT_DATA+i)*DATA_WIDTH+:DATA_WIDTH], inner_data_wire[(5*NUM_OUTPUT_DATA+i)*DATA_WIDTH+:DATA_WIDTH], inner_data_wire[(4*NUM_OUTPUT_DATA+i)*DATA_WIDTH+:DATA_WIDTH], inner_data_wire[(3*NUM_OUTPUT_DATA+i)*DATA_WIDTH+:DATA_WIDTH], inner_data_wire[(2*NUM_OUTPUT_DATA+i)*DATA_WIDTH+:DATA_WIDTH], inner_data_wire[(1*NUM_OUTPUT_DATA+i)*DATA_WIDTH+:DATA_WIDTH], inner_data_wire[(0*NUM_OUTPUT_DATA+i)*DATA_WIDTH+:DATA_WIDTH]};
-            assign inner_valid_i_mux_tree_wire = {inner_valid_wire[7*NUM_OUTPUT_DATA+i], inner_valid_wire[6*NUM_OUTPUT_DATA+i], inner_valid_wire[5*NUM_OUTPUT_DATA+i], inner_valid_wire[4*NUM_OUTPUT_DATA+i], inner_valid_wire[3*NUM_OUTPUT_DATA+i], inner_valid_wire[2*NUM_OUTPUT_DATA+i], inner_valid_wire[1*NUM_OUTPUT_DATA+i], inner_valid_wire[0*NUM_OUTPUT_DATA+i]};
-            assign inner_cmd_i_mux_tree_wire = {i_cmd_id[7].inner_cmd_wire[i], i_cmd_id[6].inner_cmd_wire[i], i_cmd_id[5].inner_cmd_wire[i], i_cmd_id[4].inner_cmd_wire[i], i_cmd_id[3].inner_cmd_wire[i], i_cmd_id[2].inner_cmd_wire[i], i_cmd_id[1].inner_cmd_wire[i], i_cmd_id[0].inner_cmd_wire[i]};
+            assign inner_data_i_mux_tree_wire = {o_inner_data_wire[i*NUM_OUTPUT_DATA*DATA_WIDTH+:NUM_OUTPUT_DATA*DATA_WIDTH]};
+            assign inner_valid_i_mux_tree_wire = {o_inner_valid_wire[i*NUM_OUTPUT_DATA+:NUM_OUTPUT_DATA]};
+            assign inner_cmd_i_mux_tree_wire = {o_inner_cmd_wire[i*NUM_OUTPUT_DATA+:NUM_OUTPUT_DATA]};
 
             mux_tree_8_1_seq #(
                 .NUM_INPUT_DATA(8), 
@@ -2911,8 +2903,8 @@ module crossbar_8_8_seq#(
                 .rst(rst),
                 .i_valid(inner_valid_i_mux_tree_wire),
                 .i_data_bus(inner_data_i_mux_tree_wire),
-                .o_valid(o_valid_wire[i]),
-                .o_data_bus(o_data_bus_wire[i*DATA_WIDTH+:DATA_WIDTH]),
+                .o_valid(o_valid[i]),
+                .o_data_bus(o_data_bus[i*DATA_WIDTH+:DATA_WIDTH]),
                 .i_en(i_en),
                 .i_cmd(inner_cmd_i_mux_tree_wire)
             );
@@ -2920,17 +2912,7 @@ module crossbar_8_8_seq#(
 
     endgenerate
 
-    always @(posedge clk) 
-    begin
-        o_data_bus_reg <= o_data_bus_wire;
-        o_valid_reg <= o_valid_wire;
-    end
-
-    assign o_data_bus = o_data_bus_reg;
-    assign o_valid = o_valid_reg;
-
 endmodule
-
 
 module wire_binary_tree_1_8_seq #(
 	parameter DATA_WIDTH = 32,      // could be arbitrary number
